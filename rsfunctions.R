ebdd59fb1eda88ccb59b395a5ffb771fa0e5629e
Zeynel Cebeci <cebeciz@gmail.com>
2019-11-17 11:51:48 +0300

    Add files via upload

--- rsfunctions.R
+++ rsfunctions.R
@@ -21,3 +21,48 @@ calc.desc <- function(x, indices, gama=0.1){
    class(descstats) <- "desc"
    return(descstats)
 }
+
+# Fonksiyon 3.2: Açýklayýcý istatistikleri görüntüleme fonksiyonu 
+print.desc <- function(x){
+    cat("Ortalama : ", x$mean, "\n")
+    cat("Ortanca  : ", x$med, "\n")
+    cat("Bud.Ort. : ", x$trmean, "\n")
+    cat("Min      : ", x$min, "\n")
+    cat("Max      : ", x$max, "\n")
+    cat("Q1       : ", x$Q1, "\n")
+    cat("Q3       : ", x$Q3, "\n")
+    cat("IQR      : ", x$IQR, "\n")
+    cat("Varyans  : ", x$var, "\n")
+    cat("Std.sapma: ", x$sd, "\n")
+    cat("Std.Hata : ", x$se, "\n")
+    cat("Var.Kat.%: ", x$cv, "\n")
+ }
+
+# Fonksiyon 3.3: Açýklayýcý istatistik grafikleri
+# Baðýmlýlýk – Paket: vioplot
+univar.plot <- function(x){
+    par(mfrow=c(3,2))
+    hist(x, col="gray90", prob=TRUE, ylab="Yoðunluk",
+      main="Histogram")
+    lines(density(x), col=4)
+    if(!require(vioplot, quietly=TRUE)){ 
+       boxplot(x, col="gray90", bg=3, xlab="x",
+         main="Kutu-býyýk grafiði")
+    }else{
+       vioplot::vioplot(x, col="gray90", bg=3,
+         xlab="", main="Keman grafiði")
+    }
+     plot(x, col=1, bg="gray90", xlab="", main="Serpilme grafiði")
+    abline(h=median(x), col=2)
+    abline(h=mean(x), col=4)
+    stripchart(x, col=1, bg="gray90", pch=21, cex=1, 
+      xlab="x", main="Þerit grafik")
+    plot.ecdf(x, col=1, bg="gray90", main="ECDF")
+    curve(pnorm(x,mean(x), sd(x)), col=4, add=TRUE)
+    qqnorm(x, datax=TRUE, pch=21, col=1, bg="gray90",
+       main="Normal Q-Q grafiði")
+    qqline(x, datax=TRUE, col=4,lwd=1) # Q-Q doðrusu
+ }
+
+
+
